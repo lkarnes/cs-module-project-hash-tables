33f4c8feca1cfb8fe5812cce0a346a8081ecec77
Logan Karnes <logan.karnes98@gmail.com>
2020-07-14 21:05:10 -0700

    day one complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,22 @@
+whitelist = set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'\r\n\t")
 def word_count(s):
-    # Your code here
-
-
+    cache = {}
+    print(s)
+    s.replace("\r",' ')
+    s.replace("\t",' ')
+    s.replace("\n",' ')
+    print(s)
+    s = ''.join(filter(whitelist.__contains__, s))
+    if s != '':
+        arr = s.split(" ")
+        for i in arr:
+            if i != "":
+                if i.lower() in cache:
+                    cache[i.lower()] += 1
+                else:
+                    cache[i.lower()] = 1
+    print(cache, s)
+    return cache
 
 if __name__ == "__main__":
     print(word_count(""))
