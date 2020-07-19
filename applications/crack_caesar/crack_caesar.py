b4a6094ccb240661fad200ec4bbcacf431d90f86
Logan Karnes <logan.karnes98@gmail.com>
2020-07-19 14:46:06 -0700

    finished caesar;

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,6 @@
 file = open('./ciphertext.txt','r+')
 tester = file.read().replace("\n", " ")
+whitelist = set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 frequency = {
     "E" :11.53,"T" :9.75,"A" :8.46,"O" :8.08,"H" :7.71,"N" :6.73,"R" :6.29,
     "I" :5.84,"S" :5.56,"D" :4.74,"L" :3.92,"W" :3.08,"U" :2.59,"G" :2.48,
@@ -9,16 +10,27 @@ frequency = {
 
 
 def decryptor(s):
+    s = ''.join(filter(whitelist.__contains__, s))
+    decrypted = {}
     commanality = {}
     length = len(s)
-    res = ''
     letters = [letter.upper() for letter in s]
     for i in letters:
         if i in commanality:
-            commanality[i] += 1
+            commanality[i] = commanality[i]+1
         else:
-            commanality[i] = 1
-    for key in commanality:
-        print(key ,commanality[key]/length)
-print(decryptor('test'))
+            if i != ' ':
+                commanality[i] = 1
+    sorted_list = [ k for k,v in sorted(commanality.items(),reverse = True, key = lambda item: item[1])]
+    frequency_list = ["E","T","A","O","H","N","R","I","S","D","L","W","U","G","F","B" ,"M","Y","C","P","K","V","Q","J","X","Z"]
+    for index, i in enumerate(sorted_list):
+        decrypted[i] = frequency_list[index]
+    response = []
+    for j in s:
+        if j == ' ':
+            response.append(' ')
+        else:
+            response.append(decrypted[j.upper()])
+    return ''.join(response)
+print(decryptor('testz'))
 print(decryptor(tester))
